987e04565b0515a06a3205b58c7175896668f36f
commander-z3r0 <cz3r0.pro@proton.me>
2026-01-26 01:26:30 +0100

    some correction on include/parrot-core-minimal.sh

--- include/parrot-core-minimal.sh
+++ include/parrot-core-minimal.sh
@@ -13,8 +13,34 @@ echo "[+] Installing Parrot keyring"
 wget -q https://deb.parrot.sh/parrot/pool/main/p/parrot-archive-keyring/parrot-archive-keyring_2024.12_all.deb
 apt-get install -y ./parrot-archive-keyring_2024.12_all.deb
 
-echo "[+] Adding Parrot repository"
-echo "deb https://deb.parrot.sh/parrot rolling main" > /etc/apt/sources.list.d/parrot.list
+echo "[+] Adding repositories"
+
+# Parrot repositories (release 'echo')
+cat <<EOF > /etc/apt/sources.list.d/parrot.list
+deb https://deb.parrot.sh/parrot echo main contrib non-free non-free-firmware
+deb https://deb.parrot.sh/parrot echo-security main contrib non-free non-free-firmware
+deb https://deb.parrot.sh/parrot echo-backports main contrib non-free non-free-firmware
+EOF
+
+# Mobian repository (assume already in sources.list.d/mobian.list)
+cat <<EOF > /etc/apt/sources.list.d/mobian.list
+deb https://repo.mobian.org forky main
+EOF
+
+echo "[+] Setting APT pinning"
+# Parrot: priority 700
+cat <<EOF > /etc/apt/preferences.d/99-parrot
+Package: *
+Pin: origin deb.parrot.sh
+Pin-Priority: 700
+EOF
+
+# Mobian: priority 600
+cat <<EOF > /etc/apt/preferences.d/99-mobian
+Package: *
+Pin: origin repo.mobian.org
+Pin-Priority: 600
+EOF
 
 echo "[+] Installing Parrot core"
 apt-get update
